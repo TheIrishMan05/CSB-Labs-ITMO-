555f7c8259691953e19f4c6f5655b420e9e688b0
Григорий Андреевич Машкин <143895718+TheIrishMan05@users.noreply.github.com>
2024-04-04 04:11:40 +0300

    Add files via upload

---
+++ Lab5/package-search-index.js
@@ -0,0 +1,5 @@
+packageSearchIndex = [{
+    "l": "All Packages",
+    "u": "allpackages-index.html"
+}, {"l": "ru.ifmo"}, {"l": "ru.ifmo.commands"}, {"l": "ru.ifmo.exceptions"}, {"l": "ru.ifmo.managers"}, {"l": "ru.ifmo.models"}, {"l": "ru.ifmo.utils"}];
+updateSearchResults();

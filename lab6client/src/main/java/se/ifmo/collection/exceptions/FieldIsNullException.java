5f71b11800dfaad9935b71244864d12a2800e8b9
Григорий Андреевич Машкин <143895718+TheIrishMan05@users.noreply.github.com>
2024-06-09 02:36:40 +0300

    Delete lab6client directory

--- lab6client/src/main/java/se/ifmo/collection/exceptions/FieldIsNullException.java
+++
@@ -1,7 +0,0 @@
-package se.ifmo.collection.exceptions;
-
-public class FieldIsNullException extends RuntimeException {
-    public FieldIsNullException(String fieldName) {
-        super("Error: Field " + fieldName + " is null!");
-    }
-}
